717896986511b96f8157f7c9789bd4399275b809
Camila Cisnero <antocisnero@gmail.com>
2026-06-30 16:24:36 -0300

    Add save login user data in sharedPrefs

--- lib/features/profile/views/profile_screen.dart
+++ lib/features/profile/views/profile_screen.dart
@@ -31,23 +31,8 @@ class ProfileScreen extends StatelessWidget {
           PopupMenuButton<String>(
             icon: const Icon(Icons.settings_outlined, color: Colors.black),
             color: Colors.white,
-
             onSelected: (value) {
-              switch (value) {
-                case 'edit':
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => const EditProfileScreen(),
-                    ),
-                  );
-                  break;
-
-                case 'logout':
-                  authProvider.logout();
-                  Navigator.popUntil(context, (route) => route.isFirst);
-                  break;
-              }
+              _handleMenuAction(context, value);
             },
             itemBuilder: (context) => [
               const PopupMenuItem(
@@ -171,5 +156,23 @@ class ProfileScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<void> _handleMenuAction(BuildContext context, String value) async {
+    final authProvider = context.read<AuthProvider>();
+    switch (value) {
+      case 'edit':
+        await Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => const EditProfileScreen()),
+        );
+        break;
+
+      case 'logout':
+        await authProvider.logout();
+        if (!context.mounted) return;
+        Navigator.popUntil(context, (route) => route.isFirst);
+        break;
+    }
   }
 }
